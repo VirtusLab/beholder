12d5fff64dedfdf11959d3b6bf85510fd3b5a9ff
Krzysztof Romanowski <romanowski.kr@gmail.com>
2014-03-24 11:40:57 +0100

    commets

--- src/test/scala/org/virtuslab/beholder/UserMachinesView.scala
+++ src/test/scala/org/virtuslab/beholder/UserMachinesView.scala
@@ -8,23 +8,27 @@ import play.api.db.slick.Config.driver.simple._
  * Author: Krzysztof Romanowski
  */
 trait UserMachinesView {
+
   case class UserMachineView(email: String,
                              system: String,
                              cores: Int)
 
 
   def createUsersMachineView() = {
+    //query that is a base for view
     val usersMachinesQuery = for {
       user <- Users
       userMachine <- UserMachines if user.id === userMachine.userId
       machine <- Machines if machine.id === userMachine.machineId
     } yield (user, machine)
 
-    FilterableViews.createView("USERS_MACHINE_VIEW",
-      UserMachineView.apply _,
-      UserMachineView.unapply _,
-      usersMachinesQuery) {
+
+    FilterableViews.createView(name = "USERS_MACHINE_VIEW",
+      apply = UserMachineView.apply _,
+      unapply = UserMachineView.unapply _,
+      baseQuery = usersMachinesQuery) {
       case (user, machine) =>
+        //naming the fields
         ("email" -> user.email,
           "system" -> machine.system,
           "cores" -> machine.cores)
